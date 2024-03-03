79354c351f945f663c6e1ba618a1487d1c4fbb1c
menthy-wu <wumengxuan0728@gmail.com>
2024-03-02 20:07:12 -0800

    gesture

---
+++ gesture.py
@@ -0,0 +1,30 @@
+import cv2
+import tensorflow as tf
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import numpy as np
+import base64
+from flask import jsonify 
+
+base_options = python.BaseOptions(model_asset_path='gesture_recognizer.task')
+options = vision.GestureRecognizerOptions(base_options=base_options)
+recognizer = vision.GestureRecognizer.create_from_options(options)
+
+
+
+async def gesture(base64Image):
+    binary = base64.b64decode(base64Image)
+    image = np.asanyarray(bytearray(binary), dtype=np.uint8)
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    image = mp.Image( image_format=mp.ImageFormat.SRGB, data=np.asarray(image))
+
+    recognition_result = recognizer.recognize(image)
+    gesture = "No gesture detected"
+
+    try:
+        gesture = (recognition_result.gestures[0][0].category_name)
+    except:
+        gesture = "No gesture detected"
+    
+    return  jsonify(gesture=gesture)
--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, request
 
 from drawsy import drawsy
+from gesture import gesture
 
 app = Flask(__name__)
 
@@ -10,6 +11,12 @@ app = Flask(__name__)
 async def drawsyapi():
     image = request.form
     result = await drawsy(image['image'])
+    return result
+
+@app.route("/gesture", methods=["POST"])
+async def gestureapi():
+    image = request.form
+    result = await gesture(image['image'])
     return result
 
 app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
